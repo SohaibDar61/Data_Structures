ba279fedcea10db1c5208dc176c9aece7d4c69f0
Sohaib Dar <63453547+SohaibDar61@users.noreply.github.com>
2022-03-09 18:45:52 +0200

    Update STACK.h

--- CODE/STACK Implementation/Stack  ( Array based USING pointer)/STACK.h
+++ CODE/STACK Implementation/Stack  ( Array based USING pointer)/STACK.h
@@ -4,22 +4,24 @@
 #include "STACK_Type.h"
 #define MAX_STACK 10
 
-typedef enum{
+typedef enum status_stack{
     STACK_NOT_FULL,
     STACK_FULL,
     STACK_Empty,
     DONE
 }STATUS_STACK;
 
+typedef enum return_status{
+    R_NOK,
+    R_OK
+}return_status_t;
+
 typedef struct Stack{
-  int entry[MAX_STACK];
-  int Top;
-  int Size;
+  uint32_t entry[MAX_STACK];
+  sint16_t Top;
+  uint32_t Size;
 }STACK;
 
-typedef unsigned char uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned int uint32_t;
 
 STATUS_STACK CreateStack(STACK *PS);
 STATUS_STACK ClearStack(STACK *PS);
